deeab654ba82ad7de3ab4b839a548170bc4ff201
Flavius Lacatusu <flacatus@redhat.com>
2024-01-19 16:14:03 +0100

    Update main.go

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 func HelloServer(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path[1:]
 	if path != "" {
-		fmt.Fprintf(w, "Helaaasssassslssso, %s!", r.URL.Path[1:])
+		fmt.Fprintf(w, "Helaaasssassslsssssso, %s!", r.URL.Path[1:])
 	} else {
 		fmt.Fprint(w, "Hello World!")
 	}
